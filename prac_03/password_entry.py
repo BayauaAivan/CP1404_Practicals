5d8692e9176aba9f2bacb1845a0e0ac14f8b7e9d
BayauaAivan <aivan.bayaua@my.jcu.edu.au>
2019-08-29 09:56:49 +1000

    changed program to utilise functions

--- prac_03/password_entry.py
+++ prac_03/password_entry.py
@@ -1,9 +1,26 @@
 """Aivan"""
 
 MIN_LENGTH = 4
+#
+# password = input("Please enter a password of at least {} characters: ".format(MIN_LENGTH))
+# while len(password) < MIN_LENGTH:
+#     password = input("Please enter a password of at least {} characters: ".format(MIN_LENGTH))
+#
+# print("*" * len(password))
 
-password = input("Please enter a password of at least {} characters: ".format(MIN_LENGTH))
-while len(password) < MIN_LENGTH:
-    password = input("Please enter a password of at least {} characters: ".format(MIN_LENGTH))
+def main():
+    password = get_password(MIN_LENGTH)
+    print_asterisks(password)
 
-print("*" * len(password))
+def get_password(min_length):
+    password = input("Please enter password of at least {} characters: ".format(min_length))
+    while len(password) < min_length:
+        print("Password is too short")
+        password = input("Please enter password of at least {} characters: ".format(min_length))
+    return password
+
+def print_asterisks(sequence):
+    print('*' * len(sequence))
+
+
+main()
