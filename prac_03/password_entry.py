ea16d9f80c7f1c446de40b48c57df4a308467140
BayauaAivan <aivan.bayaua@my.jcu.edu.au>
2019-08-29 12:57:29 +1000

    fixed formatting

--- prac_03/password_entry.py
+++ prac_03/password_entry.py
@@ -1,6 +1,8 @@
 """Aivan"""
 
 MIN_LENGTH = 4
+
+
 #
 # password = input("Please enter a password of at least {} characters: ".format(MIN_LENGTH))
 # while len(password) < MIN_LENGTH:
@@ -12,12 +14,14 @@ def main():
     password = get_password(MIN_LENGTH)
     print_asterisks(password)
 
+
 def get_password(min_length):
     password = input("Please enter password of at least {} characters: ".format(min_length))
     while len(password) < min_length:
         print("Password is too short")
         password = input("Please enter password of at least {} characters: ".format(min_length))
     return password
+
 
 def print_asterisks(sequence):
     print('*' * len(sequence))
