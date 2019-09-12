51db6df78e0a7a12c3b6bd85b97cda17a349354a
BayauaAivan <aivan.bayaua@my.jcu.edu.au>
2019-09-12 10:16:02 +1000

    added dictionary

---
+++ prac_05/hex_colours.py
@@ -0,0 +1,9 @@
+"""
+CP1404 Practical
+make a hex colour code dictionary
+invalid option should not crash
+entering '' should end loop
+"""
+
+COLOUR_CODES = {"aliceblue": "#f0f8ff", "antiqueWhite": "", "antiqueWhite1": "", "antiqueWhite2": "", "antiqueWhite3": "", "antiqueWhite4": "", "aquamarine1": "", "aquamarine2": ""}
+
