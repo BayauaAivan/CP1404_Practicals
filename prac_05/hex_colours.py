f24df5e80b341a30eba8309e809ca6141a714880
BayauaAivan <aivan.bayaua@my.jcu.edu.au>
2019-09-12 10:32:15 +1000

    added loop

--- prac_05/hex_colours.py
+++ prac_05/hex_colours.py
@@ -5,5 +5,13 @@ invalid option should not crash
 entering '' should end loop
 """
 
-COLOUR_CODES = {"aliceblue": "#f0f8ff", "antiqueWhite": "", "antiqueWhite1": "", "antiqueWhite2": "", "antiqueWhite3": "", "antiqueWhite4": "", "aquamarine1": "", "aquamarine2": ""}
+COLOUR_CODES = {"aliceblue": "#f0f8ff", "antiquewhite": "#faebd7", "antiquewhite1": "#ffefdb", "antiquewhite2": "#eedfcc",
+                "antiquewhite3": "#cdc0b0", "antiquewhite4": "#8b8378", "aquamarine1": "#7fffd4", "aquamarine2": "#76eec6"}
 
+colour = (input("Enter colour: ")).lower()
+while colour != "":
+    if colour in COLOUR_CODES:
+        print(colour, "hex colour code is", COLOUR_CODES[colour])
+    else:
+        print("Invalid colour")
+    colour = (input("Enter colour: ")).lower()
